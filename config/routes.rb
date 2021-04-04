54de9fcd81ea23210280277fbf1ab1e7b4be4177
Ben Prat <bvprat@gmail.com>
2021-04-04 15:28:16 -0600

    "Create Index Route"

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/photos" => "photos#index"
+  end
 end
