d2ab2416f5cd55642908e8bf9d1ac985b80ae5e9
Ben Prat <bvprat@gmail.com>
2021-04-05 11:52:35 -0600

    "Add create wine action"

--- app/controllers/api/wines_controller.rb
+++ app/controllers/api/wines_controller.rb
@@ -3,5 +3,19 @@ class Api::WinesController < ApplicationController
     @wines = Wine.all
     render "index.json.jb"
   end
+
+  def create
+    @wine = Wine.new(
+    style: params[:style],
+    name: params[:name],
+    producer: params[:producer],
+    variety: params[:variety],
+    location: params[:location],
+    vintage: params[:vintage]
+  )
+    @wine.save
+    render "show.json.jb"
+  end
+
 end
 
