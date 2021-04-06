e6df0f7441733ec64a0b5e89920d1c52f0f0e8ff
Ben Prat <bvprat@gmail.com>
2021-04-06 13:08:33 -0600

    "Add validations and secure_pword method"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
   has_many :entries
   has_many :wines, through: :entries
 
