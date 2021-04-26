7a77f8ee93358d46092df27afdff579edb909440
Ben Prat <bvprat@gmail.com>
2021-04-26 14:07:53 -0600

    Seed redgrape db

--- app/controllers/api/redgrapes_controller.rb
+++ app/controllers/api/redgrapes_controller.rb
@@ -1,2 +1,24 @@
 class Api::RedgrapesController < ApplicationController
+
+  def create
+    @redgrape = RedGrape.new(
+    name: params:[:name],
+    pronunciation: params:[:pronunciation],
+    origin: params:[:origin],
+    fruit: params:[:fruit],
+    body: params:[:body],
+    tannin: params:[:tannin],
+    acidity: params:[:acidity],
+    alcohol: params:[:alcohol],
+    dominant_flavors: params:[:dominant_flavors],
+    )
+    @redgrape.save
+    render "show.json.jb"
+  end
+
+
 end
+
+
+
+
