d7d11f9e30e37a6df4f7a6be2645d6eb623d8a3f
Ben Prat <bvprat@gmail.com>
2021-04-26 10:41:38 -0600

    Fix users controller error

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,17 +1,19 @@
 class Api::UsersController < 
-ApplicationController
-  def create
-    user = User.new(
-    first_name: params[:first_name],
-    email: params[:email],
-    password: params[:password],
-    password_validation: params[:password_validation]
-)
-    if user.save
-      render json: { message: "User created successfully" }, status: :created
- else
-   render json: { errors: user.errorsfull_messages }, status: :bad_request
+  ApplicationController
+    def create
+      user = User.new(
+      first_name: params[:first_name],
+      email: params[:email],
+      password: params[:password],
+      password_validation: params[:password_validation]
+  )
+      if user.save
+        render json: { message: "User created successfully" }, status: :created
+   else
+     render json: { errors: user.errors.full_messages }, status: :bad_request
+      end
     end
+  
   end
 
-end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :entries
   has_many :wines, through: :entries
-  has_secure_password
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_validation, presence:true
+  validates :password_validation, presence: true
   
 
 end
