b4eb6895197e5b24444192f74071ef046d402792
Ben Prat <bvprat@gmail.com>
2021-04-26 15:18:59 -0600

    Finish Full Bodied Red Wine

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-redgrapes = RedGrape.create!([{name: "Valpolicella Blend", pronunciation: "val-polla-chella", 
-origin: "Italy", fruit: 3, body: 3, tannin: 2, acidity: 5, alcohol: 3, 
-dominant_flavors: "Sour Cherry Cinnamon Green Peppercorn Carob Green Almond"},
-{name:"Zinfandel", pronunciation: "zin-fan-dell", origin:"Croatia", fruit: 5, body: 5, 
-tannin: 3, acidity: 2, alcohol: 5, dominant_flavors: "Blackberry, Strawberry, Peach Preserves 5-Spice Powder Sweet Tobacco"}, 
-{name:"Aglianico", pronunciation: "alli-yawn-nico", 
-origin:"Italy", fruit: 2, body: 5, tannin: 5, acidity: 5, alcohol: 4, dominant_flavors: 
-"White Pepper Black Cherry Smoke Game Spiced Plum"}])
+redgrapes = RedGrape.create!([{name: "Touriga Nacional", pronunciation: "tor-ee-gah nah-see-un-nall", 
+origin: "Portugal", fruit: 5, body: 5, tannin: 5, acidity: 4, alcohol: 5, 
+dominant_flavors: "Violet Blueberry Plum Mint Wet Slate"}])
