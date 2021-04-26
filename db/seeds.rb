f4471036a14be4afb2649071415632931b396330
Ben Prat <bvprat@gmail.com>
2021-04-26 14:08:04 -0600

    Seed red grape db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-grapes = Grape.create!([{name: "Rosé", pronunciation: "rose-aye", 
-origin: "unknown", fruit: 5 , body: 3, dry: 2, acidity: 3, alcohol: 3, 
-dominant_flavors: "Strawberry Honeydew/Melon Rose Petal Celery Orange Peel"},
-{name:"Gamay", pronunciation: "gam-may", origin:"France", fruit: 3, body: 2, 
-dry: 1, acidity: 4, alcohol: 2, dominant_flavors: "Huckleberry Raspberry Violet Potting Soil Banana"}, 
-{name:"Pinot Noir", pronunciation: "pee-no nwar", 
-origin:"France", fruit: 4, body: 3, dry: 2, acidity: 4, alcohol: 3, dominant_flavors: 
-"Cranberry Cherry Raspberry Clove Mushroom"}])
+redgrapes = RedGrape.create!([{name: "Pinot Noir", pronunciation: "pee-no nwar", 
+origin: "France", fruit: 4, body: 3, tannin: 2, acidity: 4, alcohol: 3, 
+dominant_flavors: "Cranberry Cherry Raspberry Clove Mushroom"},
+{name:"Barbera", pronunciation: "bar-BEAR-uh", origin:"Italy", fruit: 4, body: 4, 
+tannin: 1, acidity: 5, alcohol: 4, dominant_flavors: "Sour Cherry Licorice Blackberry Dried Herbs Tar"}, 
+{name:"Cabernet Franc", pronunciation: "cab-err-nay fronk", 
+origin:"France", fruit:3 , body: 3, tannin: 4, acidity: 4, alcohol: 3, dominant_flavors: 
+"Strawberry Roasted Pepper Red Plum Crushed Gravel Chili Pepper"}])
