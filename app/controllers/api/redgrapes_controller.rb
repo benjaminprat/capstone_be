845c91d18eb3851b024aba561178ab2389a81d3c
Ben Prat <bvprat@gmail.com>
2021-04-26 15:31:25 -0600

    Add Index and create action for red grape

--- app/controllers/api/redgrapes_controller.rb
+++ app/controllers/api/redgrapes_controller.rb
@@ -1,23 +1,28 @@
 class Api::RedgrapesController < ApplicationController
+  
+  def index 
+    @redgrapes = RedGrape.all() 
+    render 'index.json.jb'
+  end
 
   def create
     @redgrape = RedGrape.new(
-    name: params:[:name],
-    pronunciation: params:[:pronunciation],
-    origin: params:[:origin],
-    fruit: params:[:fruit],
-    body: params:[:body],
-    tannin: params:[:tannin],
-    acidity: params:[:acidity],
-    alcohol: params:[:alcohol],
-    dominant_flavors: params:[:dominant_flavors],
+    name: params[:name],
+    pronunciation: params[:pronunciation],
+    origin: params[:origin],
+    fruit: params[:fruit],
+    body: params[:body],
+    tannin: params[:tannin],
+    acidity: params[:acidity],
+    alcohol: params[:alcohol],
+    dominant_flavors: params[:dominant_flavors],
     )
     @redgrape.save
     render "show.json.jb"
   end
-
-
 end
+
+
 
 
 
