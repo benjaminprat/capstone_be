57813448ff8a5ef734dc7f7a87c07982d6a2452b
Ben Prat <bvprat@gmail.com>
2021-04-28 19:27:13 -0600

    Seed Starches Table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ingredient = Ingredient.create!([{name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"}, 
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
-  {name: "White Rice", category: "Starch", sub_category: "White Starches"},
+ingredient = Ingredient.create!([{name: "Pasta", category: "Starch", sub_category: "White Starches"},
+  {name: "Bread", category: "Starch", sub_category: "White Starches"},
+  {name: "Flour Tortillas", category: "Starch", sub_category: "White Starches"},
+  {name: "Quinoa", category: "Starch", sub_category: "Whole Wheat Grains"}, 
+  {name: "Farro", category: "Starch", sub_category: "Whole Wheat Grains"},
+  {name: "Brown Rice", category: "Starch", sub_category: "Whole Wheat Grains"},
+  {name: "Sweet Potato", category: "Starch", sub_category: "Sweet Starchy Vegetable"},
+  {name: "Yucca", category: "Starch", sub_category: "Sweet Starchy Vegetable"},
+  {name: "Taro", category: "Starch", sub_category: "Sweet Starchy Vegetable"},
+  {name: "Russet Potato", category: "Starch", sub_category: "Potato"},
+  {name: "Idaho Potato", category: "Starch", sub_category: "Potato"},
        ])
 
 
