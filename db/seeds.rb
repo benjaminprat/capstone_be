1497cebb85ae7339b008092747cc0c338c59c70a
Ben Prat <bvprat@gmail.com>
2021-04-29 22:25:26 -0600

    Seed Pairing db-sparkling-fish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 pairing = Pairing.create!([
-  {ingredient_id: 67, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 68, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 69, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 70, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 14, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 15, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 16, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 17, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 18, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 19, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 20, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 21, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 22, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 23, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 24, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 25, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 26, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 27, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 28, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 29, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 30, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 31, classification_id: 1, perfect_pairing: false},
+
+
+
 
   ])
 
