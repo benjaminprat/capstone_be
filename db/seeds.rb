ccace512e2e2e5822ab645254a33a784b5015bc8
Ben Prat <bvprat@gmail.com>
2021-04-29 22:51:29 -0600

    seed paring db-sweet-starches

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 pairing = Pairing.create!([
-  {ingredient_id: 122, classification_id: 1, perfect_pairing: true},
-  {ingredient_id: 123, classification_id: 1, perfect_pairing: true},
-  {ingredient_id: 124, classification_id: 1, perfect_pairing: true},
-  {ingredient_id: 125, classification_id: 1, perfect_pairing: true},
-  {ingredient_id: 126, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 127, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 1, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 2, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 3, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 4, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 5, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 6, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 7, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 8, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 9, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 10, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 11, classification_id: 1, perfect_pairing: false},
+
   
 
   ])
