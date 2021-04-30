96c3a56b77984a88e44234be91f719533ee976b8
Ben Prat <bvprat@gmail.com>
2021-04-29 22:39:05 -0600

    Seed Pairing db-sparkling-poultry

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 pairing = Pairing.create!([
-  {ingredient_id: 32, classification_id: 1, perfect_pairing: true},
-  {ingredient_id: 33, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 43, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 44, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 45, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 46, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 47, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 48, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 49, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 50, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 51, classification_id: 1, perfect_pairing: true},
+  {ingredient_id: 52, classification_id: 1, perfect_pairing: true},
+
   ])
 
       
