b3003a0d5d930009923276a644d3da9c0f41fbd7
Ben Prat <bvprat@gmail.com>
2021-04-29 23:46:43 -0600

    Seed Pairing db-full-white

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,83 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 pairing = Pairing.create!([
-  {ingredient_id: 12, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 13, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 1, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 2, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 3, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 4, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 5, classification_id: 4, perfect_pairing: false},
-  {ingredient_id: 88, classification_id: 4, perfect_pairing: true},
-  {ingredient_id: 89, classification_id: 4, perfect_pairing: true},
-  {ingredient_id: 90, classification_id: 4, perfect_pairing: true},
-  {ingredient_id: 91, classification_id: 4, perfect_pairing: true},
-  {ingredient_id: 92, classification_id: 4, perfect_pairing: true},
-  {ingredient_id: 93, classification_id: 2, perfect_pairing: true},
+  {ingredient_id: 12, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 13, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 1, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 2, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 3, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 4, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 5, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 6, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 7, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 8, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 88, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 89, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 90, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 91, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 92, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 93, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 78, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 79, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 80, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 81, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 82, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 83, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 84, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 85, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 86, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 87, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 63, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 64, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 65, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 66, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 67, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 68, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 69, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 70, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 25, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 26, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 27, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 28, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 29, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 30, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 31, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 19, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 20, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 21, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 22, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 23, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 24, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 40, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 41, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 42, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 34, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 35, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 36, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 37, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 38, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 39, classification_id: 5, perfect_pairing: false},
+  {ingredient_id: 46, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 47, classification_id: 5, perfect_pairing: true},
+  {ingredient_id: 48, classification_id: 5, perfect_pairing: true},
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
 
 
 
