24f2e4afbfaaa894045dbe74f9e76db180f40cc4
Ben Prat <bvprat@gmail.com>
2021-04-29 22:21:09 -0600

    Seed Pairing db- sparkling-allium

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 pairing = Pairing.create!([
-  {ingredient_id: 56, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 57, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 58, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 59, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 60, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 61, classification_id: 1, perfect_pairing: false},
-  {ingredient_id: 62, classification_id: 1, perfect_pairing: false},
-
+  {ingredient_id: 67, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 68, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 69, classification_id: 1, perfect_pairing: false},
+  {ingredient_id: 70, classification_id: 1, perfect_pairing: false},
 
   ])
 
