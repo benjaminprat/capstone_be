e0af5e3e7b16b141f47881823e3b7819e7680e74
Ben Prat <bvprat@gmail.com>
2021-05-01 10:20:24 -0600

    Add first integration test wines index

--- spec/requests/wines_spec.rb
+++ spec/requests/wines_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 RSpec.describe "Wines", type: :request do
   describe "GET /wines" do
     it "should show all wines" do
-      get wines_index_path
+
+      Wine.create!(
+        style: "the style",
+        producer_wine: "the producer_wine",
+        grape: "the grape",
+        region: "the region",
+        country: "the country",
+        vintage: "2008",
+      )
+      
+      get "/api/wines"
+      wines = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect
+      expect(wines.length).to eq(1)
     end
   end
+
+  
 end
